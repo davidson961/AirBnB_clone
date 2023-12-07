519b1b9e36d5f92273644a5a7e06b4d521e1f6fa
davidson961 <davidzamerson.9@gmail.com>
2023-12-07 08:09:47 -0800

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -14,11 +14,22 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        """Initializes a new instance of the BaseModel class."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+        If kwargs is not empty, sets attributes based on the key-value
+        pairs in kwargs.
+        Otherwise, initializes attributes with default values.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Provides a formatted string representation of the instance."""
