e14b48e0038de08fff7e95988555fdff827e65a7
davidson961 <davidzamerson.9@gmail.com>
2023-12-09 21:55:51 -0800

    adding

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""
-This module initializes the storage variable, an instance of FileStorage,
-and calls reload() on this variable.
-"""
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
-storage.reload()
