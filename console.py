3a5166a7389318b5f99c580e095c71b0b247e025
davidson961 <davidzamerson.9@gmail.com>
2023-12-10 09:34:49 -0800

    testing

---
+++ console.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python3
+"""
+Module containing the HBNBCommand class for the command interpreter.
+"""
+import cmd
+import json
+from models.base_model import BaseModel
+from models import storage
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter class.
+    """
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """
+        Empty line handler (does nothing).
+        """
+        pass
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program.
+        """
+        print()
+        return True
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file), and prints the id.
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name and id.
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) == 1:
+                print("** instance id missing **")
+                return
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            print(storage.all().get(key, "** no instance found **"))
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (saves the change into the JSON file).
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) == 1:
+                print("** instance id missing **")
+                return
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            instances = storage.all()
+            if key not in instances:
+                print("** no instance found **")
+            else:
+                del instances[key]
+                storage.save()
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name.
+        """
+        args = arg.split()
+        instances = storage.all()
+        if not args or args[0] not in storage.valid_classes():
+            print([str(obj) for obj in instances.values()])
+        else:
+            try:
+                class_name = args[0]
+                print([str(obj) for key, obj in instances.items() if class_name == key.split('.')[0]])
+            except NameError:
+                print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute.
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if class_name not in storage.valid_classes():
+                print("** class doesn't exist **")
+                return
+            if len(args) == 1:
+                print("** instance id missing **")
+                return
+            instance_id = args[1]
+            key = "{}.{}".format(class_name, instance_id)
+            instances = storage.all()
+            if key not in instances:
+                print("** no instance found **")
+            else:
+                if len(args) == 2:
+                    print("** attribute name missing **")
+                elif len(args) == 3:
+                    print("** value missing **")
+                else:
+                    attribute_name = args[2]
+                    attribute_value = args[3]
+                    instance = instances[key]
+                    try:
+                        # Try to cast the attribute value to the type of the attribute
+                        setattr(instance, attribute_name, type(getattr(instance, attribute_name))(attribute_value))
+                    except ValueError:
+                        # If casting fails, assume it's a string
+                        setattr(instance, attribute_name, attribute_value)
+                    instance.save()
+        except NameError:
+            print("** class doesn't exist **")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
---
+++ models/engine/file_storage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module defining the FileStorage class.
+"""
+import datetime
+import json
+import os
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+            s = {k: v.to_dict() for k, v in FileStorage.__objects.items()}    
+            json.dump(s, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists).
+        Otherwise, do nothing.
+        """
+        from models.base_model import BaseModel
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+            obj_dict = json.load(file)
+            obj_dict = {k: BaseModel(**v) for k, v in obj_dict.items()}
+            FileStorage.__objects = obj_dict
+    
+    def valid_classes(cls):
+        """
+        Returns a list of valid class names.
+        """
+        return [name for name in cls.__objects.keys()]
+    
