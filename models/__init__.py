4bcfcb4a4130795a99a3e59f834213ab9e6b68ef
davidson961 <125483143+davidson961@users.noreply.github.com>
2024-02-06 14:47:45 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+This script initializes an instance of the FileStorage class from the models.engine module
+and reloads data from the storage file.
+"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
