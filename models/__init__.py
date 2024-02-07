1f412c9cae2f19f001e5da71b1d695e0e49dae64
davidson961 <125483143+davidson961@users.noreply.github.com>
2024-02-07 17:00:08 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Initialization script for the FileStorage module
+
+This script creates an instance of the FileStorage class and reloads data from
+the JSON file. The FileStorage class is responsible for serializing instances
+to a JSON file and deserializing JSON files to instances.
+"""
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
+
 storage.reload()
