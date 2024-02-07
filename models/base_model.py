dc3fc87eeb7f6b972491a9b9100f366c8c30d989
davidson961 <125483143+davidson961@users.noreply.github.com>
2024-02-07 14:00:10 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,29 @@ from datetime import datetime
 class BaseModel:
     """Defines the common attributes/methods for other classes"""
 
-    def __init__(self):
-        """Initialize instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel using *args and **kwargs.
+        If it’s a new instance (not from a dictionary representation),
+        adds a call to the method new(self) on storage.
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
 
     def __str__(self):
         """Return a string representation of the instance"""
