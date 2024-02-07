fa9c973fade29574c5dd305662f5c6ef3cf1eb8a
davidson961 <125483143+davidson961@users.noreply.github.com>
2024-02-07 13:48:24 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 """
 Module to create a unique FileStorage instance for the application
 """
-
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
