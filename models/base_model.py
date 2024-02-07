dfcb486e5a3018c93aac71c68d1ac03b53c6d825
davidson961 <125483143+davidson961@users.noreply.github.com>
2024-02-07 14:07:18 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """Defines the common attributes/methods for other classes"""
@@ -28,6 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
 
     def __str__(self):
@@ -37,6 +39,7 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance"""
