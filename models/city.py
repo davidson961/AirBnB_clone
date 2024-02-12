13e783ef12feddc4f480a9ddf9d8a0de1c7f7a2f
davidson961 <davidzamerson.9@gmail.com>
2024-02-12 15:02:43 +0100

    update

--- models/city.py
+++ models/city.py
@@ -1,12 +1,9 @@
+#!/usr/bin/python3
 from models.base_model import BaseModel
 
 class City(BaseModel):
     """
     City class that inherits from BaseModel.
-
-    Public class attributes:
-        state_id (str): State ID (initialized as an empty string, linked to State.id).
-        name (str): City name (initialized as an empty string).
     """
     def __init__(self, *args, **kwargs):
         """Initialize City instance."""
