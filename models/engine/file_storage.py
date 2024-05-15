0919f8ebf54f54b04f26f6dbd23cb3ee8d58401b
davidson961 <125483143+davidson961@users.noreply.github.com>
2024-05-15 12:18:29 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,6 +40,28 @@ class FileStorage:
             res = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(res, f)
 
+    def classes(self):
+        """
+        Provides a dictionary containing valid classes and their
+        corresponding references.
+        """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """
         Deserializes the JSON file to __objects.
@@ -53,26 +75,43 @@ class FileStorage:
                         for k, v in obj_dict.items()}
             FileStorage.__objects = obj_dict
 
-    def classes(self):
-        """Provides a dictionary containing valid classes and their
-        corresponding references.
+    def attributes(self):
         """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-        
-        classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
+        Provides the valid attributes and their corresponding types for
+        the given class name.
+        """
+        attributes = {
+            "BaseModel":
+            {"id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime},
+            "User":
+            {"email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str},
+            "State":
+            {"name": str},
+            "City":
+            {"state_id": str,
+                "name": str},
+            "Amenity":
+            {"name": str},
+            "Place":
+            {"city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                "user_id": str,
+                "text": str}
             }
-        return classes
-    
+        return attributes
